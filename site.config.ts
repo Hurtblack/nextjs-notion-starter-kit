94ec3bde8a813a7bca04783fcd949799f65db447
Hurtblack <74805072+Hurtblack@users.noreply.github.com>
2022-04-11 19:21:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '72bd8fd4a0174ebd83500d051412b17e',
+  rootNotionPageId: 'HurtBlack-Blog',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
